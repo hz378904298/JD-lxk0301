9b40ba7378d38d7fc3601748b374ed32d74c1966
wuzhi04 <85538593+wuzhi04@users.noreply.github.com>
2021-07-09 13:27:43 +0800

    fix  update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -50,6 +50,8 @@
 30 2,20 * * * node /scripts/jd_joy_park.js >> /scripts/logs/jd_joy_park.log 2>&1
 #燃动夏季
 10 0,6-23/1 * * * node /scripts/jd_summer_movement.js >> /scripts/logs/jd_summer_movement.log 2>&1
+#燃动夏季SH互助
+12 7-23 * * * node /scripts/jd_summer_movement_help.js >> /scripts/logs/jd_summer_movement_help.log 2>&1
 #新签到
 10 0,18 * * * node /scripts/jd_NewSign.js >> /scripts/logs/jd_NewSign.log 2>&1
 #早起福利
