15408463be837d9f80a3a1a098f5f97adbffaf66
wuzhi04 <85538593+wuzhi04@users.noreply.github.com>
2021-08-18 13:38:56 +0800

    财富岛初始化判断修复，新增开卡7、8若不需要请注释

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -56,6 +56,10 @@
 15 0,9,18,22 * * * node /scripts/jd_xiaolong.js >> /scripts/logs/jd_xiaolong.log 2>&1
 #jd_opencard5
 25 0,9,18,22 * * * node /scripts/jd_opencard5.js >> /scripts/logs/jd_opencard5.log 2>&1
+#jd_opencard7
+15 0,12,22 * * * node /scripts/jd_opencard7.js >> /scripts/logs/jd_opencard7.log 2>&1
+#jd_opencard8
+5 0,12,22 * * * node /scripts/jd_opencard8.js >> /scripts/logs/jd_opencard8.log 2>&1
 #特物
 30 8,9 * * * node /scripts/jd_productZ4Brand.js >> /scripts/logs/jd_productZ4Brand.log 2>&1
 #女装盲盒
