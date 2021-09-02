f0917857338906274aeeb45a69fe07390f2f5362
wuzhi04 <85538593+wuzhi04@users.noreply.github.com>
2021-09-02 22:52:14 +0800

    移除过期

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -99,8 +99,6 @@
 0 10 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1
 # 京豆变动通知all
 0 21 * * * node /scripts/jd_bean_change_all.js >> /scripts/logs/jd_bean_change_all.log 2>&1
-# 京东排行榜
-11 9 * * * node /scripts/jd_rankingList.js >> /scripts/logs/jd_rankingList.log 2>&1
 # 天天提鹅
 18 * * * * node /scripts/jd_daily_egg.js >> /scripts/logs/jd_daily_egg.log 2>&1
 # 金融养猪
