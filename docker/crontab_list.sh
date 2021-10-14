6b4e5e7d7b1acec562926ff196b1c9c0a37669ef
wuzhi04 <85538593+wuzhi04@users.noreply.github.com>
2021-10-14 14:22:14 +0800

    fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -74,8 +74,8 @@
 0 1,9,23 * * * node /scripts/jd_opencard2.js >> /scripts/logs/jd_opencard2.log 2>&1
 #jd_opencard3
 45 0,12,21 * * * node /scripts/jd_opencard3.js >> /scripts/logs/jd_opencard3.log 2>&1
-jd_ddworld_exchange
-0 0,9 * * * node /scripts/jd_ddworld_exchange.js >> /scripts/logs/jd_ddworld_exchange.log 2>&1
+#jd_ddworld_exchange
+0 0,9 * * * node /scripts/jd_ddworld_exchangebean.js >> /scripts/logs/jd_ddworld_exchangebean.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
